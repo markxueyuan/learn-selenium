84331d6f421393d2b14325b6a2aa8debe2335631
Yuan Xue <ziyaajiang@gmail.com>
2014-09-16 20:29:42 +0800

    more

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [clj-webdriver "0.6.0"]
-                           ;[org.seleniumhq.selenium/selenium-java "2.42.2"]
+                           [org.seleniumhq.selenium/selenium-java "2.42.2"]
                            [org.seleniumhq.selenium/selenium-firefox-driver "2.42.2"]
-                           ;[org.seleniumhq.selenium/selenium-server "2.42.2"]
-                           ;[com.opera/operadriver "1.5"]
-                           ;[com.github.detro.ghostdriver/phantomjsdriver "1.0.4"]
+                           [org.seleniumhq.selenium/selenium-server "2.42.2"]
+                           [com.opera/operadriver "1.5"]
+                           [com.github.detro.ghostdriver/phantomjsdriver "1.0.4"]
                            ])
